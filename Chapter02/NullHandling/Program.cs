2ac07e7f76c6ab7ea06181414350835018dce8e9
Zachary Steiger <zsteig93@gmail.com>
2019-11-15 13:24:59 -0500

    Nullable value types

---
+++ Chapter02/NullHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿// #nullable enable
+// #nullable disable // Enables || disables at file level
+
+using System;
+
+namespace NullHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int thisCannotBeNull = 4;
+            // thisCannotBeNull = null; compile error!
+
+            int? thisCouldBeNull = null;
+            Console.WriteLine(thisCouldBeNull); // prints blank line because value is null
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            thisCouldBeNull = 7;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+        }
+    }
+}
