8dbfe7b2e2a7424713e1fe7f2c1903da9ff3bee9
ZSteiger <z.steig93@gmail.com>
2019-11-14 19:28:19 -0500

    Important differences between doubles and decimals

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -23,9 +23,36 @@ namespace Numbers {
 
             // check the three variables have the same value
             // both statements output true
-            Console.WriteLine ($"{decimalNotation == binaryNotation}");
-            Console.WriteLine ($"{decimalNotation == hexadecimalNotation}");
+            // Console.WriteLine ($"{decimalNotation == binaryNotation}");
+            // Console.WriteLine ($"{decimalNotation == hexadecimalNotation}");
 
+            Console.WriteLine ($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}");
+            Console.WriteLine ($"int uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}");
+            Console.WriteLine ($"int uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
+
+            Console.WriteLine ("Using doubles:");
+
+            double a = 0.1;
+            double b = 0.2;
+
+            if (a + b == 0.3) {
+                Console.WriteLine ($"{a} + {b} equals 0.3");
+            } else {
+                Console.WriteLine ($"{a} + {b} does NOT equal 0.3");
+
+            }
+            // because they do not equal, only use doubles when accuracy isn't important
+            Console.WriteLine ("Using doubles:");
+
+            decimal c = 0.1M; // M Suffix means a decimal literal value
+            decimal d = 0.2M;
+
+            if (c + d == 0.3M) {
+                Console.WriteLine ($"{a} + {b} equals 0.3");
+            } else {
+                Console.WriteLine ($"{a} + {b} does NOT equal 0.3");
+
+            }
         }
     }
 }
