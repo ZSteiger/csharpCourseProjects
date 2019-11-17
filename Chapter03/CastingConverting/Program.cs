990a969a7dff9af0440a3ff45e0c288780460105
ZSteiger <z.steig93@gmail.com>
2019-11-16 21:10:01 -0500

    Onto casting & converting

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace CastingConverting
 {
@@ -6,7 +7,13 @@ namespace CastingConverting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 10;
+            double b = a; // an int can be safely cast into a double
+            WriteLine(b);
+
+            double c = 9.8;
+            int d = c; // compiler gives an error for this line
+            WriteLine(d);
         }
     }
 }
