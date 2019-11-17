4fa1f5c31991653b9df9c31c99e403dcd57c933a
ZSteiger <z.steig93@gmail.com>
2019-11-17 11:18:20 -0500

    Writing functions

---
+++ Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using static System.Console;
+
+namespace WritingFunctions
+{
+    class Program
+    {
+        static void TimesTable(byte number)
+        {
+            for (int row = 1; row <= 12; row++)
+            {
+                WriteLine($"{row} x {number} = {row * number}");
+            }
+            WriteLine();
+        }
+        static void RunTimesTable()
+        {
+            bool isNumber;
+            do
+            {
+                Write("Enter a number between 0 and 255");
+                isNumber = byte.TryParse(
+                    ReadLine(), out byte number);
+                if (isNumber)
+                {
+                    TimesTable(number);
+                }
+                else
+                {
+                    WriteLine("You did not enter a valid number");
+                }
+            }
+            while (isNumber);
+        }
+
+        static Decimal CalculateTax(
+            decimal amount, string twoLetterRegionCode
+        )
+        {
+            decimal rate = 0.0M;
+            switch (twoLetterRegionCode)
+            {
+                case "CH": // Switz
+                    rate = 0.08M;
+                    break;
+                case "DK": // Denmark
+                case "NO": // Norway
+                    rate = 0.25M;
+                    break;
+                case "GB": // United Kingdom
+                case "FR": // France
+                    rate = 0.2M;
+                    break;
+                case "HU": // Hungary
+                    rate = 0.27M;
+                    break;
+                case "OR": // Oregon
+                case "AK": // Alaska
+                case "MT": // Montana
+                    rate = 0.0M; break;
+                case "ND": // North Dakota
+                case "WI": // Wisconsin
+                case "ME": // Maryland
+                case "VA": // Virginia
+                    rate = 0.05M;
+                    break;
+                case "CA": // Cali
+                    rate = .0825M;
+                    break;
+                default: // most US states
+                    rate = 0.6M;
+                    break;
+            }
+            return amount * rate;
+        }
+
+        static void RunCalculateTax()
+        {
+            Write("Enter an amount: ");
+            string amountInText = ReadLine();
+            Write("Enter a two-letter region code: ");
+            string region = ReadLine();
+
+            if (decimal.TryParse(amountInText, out decimal amount))
+            {
+                decimal taxToPay = CalculateTax(amount, region);
+                WriteLine($"You must pay {taxToPay} in sales tax.");
+            }
+            else
+            {
+                WriteLine("You did not enter a valid amount!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            RunTimesTable();
+        }
+    }
+}
+
