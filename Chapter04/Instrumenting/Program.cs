4e6cc72fa0a1dd189ae08be90d826da13809572e
ZSteiger <z.steig93@gmail.com>
2019-11-19 15:17:50 -0500

    Logging and instrumenting

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace Instrumenting
 {
@@ -6,7 +8,25 @@ namespace Instrumenting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // write to a text file in the project folder
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+            // text writer is buffered, so this option calls Flush() on all listeners after writing
+            Trace.AutoFlush = true;
+            Debug.WriteLine("Debug says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var ts = new TraceSwitch(displayName: "PacktSwitch", description: "This switch is set via a JSON config.");
+
+            configuration.GetSection("PacktSwitch").Bind(ts);
+
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
         }
     }
 }
