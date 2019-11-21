56b3201533aa8f57fb07d75ef9cc70501c8ce670
ZSteiger <z.steig93@gmail.com>
2019-11-20 21:20:39 -0500

    Progress save

---
+++ Chapter05/PacktLibrary/WondersOfTheAncientWorld.cs
@@ -0,0 +1,14 @@
+namespace Packt.Shared
+{
+    public enum WondersOfTheAncientWorld
+    {
+        GreatPyramidOfGiza,
+        HangingGardensOfBabylon,
+        StatusOfZeusAtOlympia,
+        TempleOfArtemisAtEphesus,
+        MausoleumAtHalicarnassus,
+        ColossusOfRhodes,
+        LighthouseOfAlexandria
+
+    }
+}
