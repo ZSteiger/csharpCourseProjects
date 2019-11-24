c79d7a596b72a63a4b6b8f9a55043d3a95a13677
ZSteiger <z.steig93@gmail.com>
2019-11-23 21:19:31 -0500

    up next: Defining Indexers

--- Chapter05/PacktLibrary/PersonAutoGen.cs
+++ Chapter05/PacktLibrary/PersonAutoGen.cs
@@ -13,5 +13,27 @@ namespace Packt.Shared
         // two properties defined using C# 6+ lambda expression syntax
         public string Greeting => $"{Name} says 'Hello!'";
         public int Age => System.DateTime.Today.Year - DateOfBirth.Year;
+        public string FavoriteIceCream { get; set; } // auto-syntax
+        private string favoritePrimaryColor;
+        public string FavoritePrimaryColor
+        {
+            get
+            {
+                return favoritePrimaryColor;
+            }
+            set
+            {
+                switch (value.ToLower())
+                {
+                    case "red":
+                    case "green":
+                    case "blue":
+                        favoritePrimaryColor = value;
+                        break;
+                    default:
+                        throw new System.ArgumentException($"{value} is not a primary color. " + "Choose from : red, green, blue.");
+                }
+            }
+        }
     }
 }
