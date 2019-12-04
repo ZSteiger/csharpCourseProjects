7f1ebc59b09751e025e94e47d89251845522c60d
ZSteiger <z.steig93@gmail.com>
2019-12-04 16:59:06 -0500

    Chapter07, publishing a self contained app

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new XDocument();
+            string s1 = "Hello";
+            string s2 = "World";
+        }
+    }
+}
