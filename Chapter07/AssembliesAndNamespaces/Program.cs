029bfecafa47d08fd53dc1fe87c0e3368bc90e7f
ZSteiger <z.steig93@gmail.com>
2019-12-04 18:31:51 -0500

    Using non-dotnet standard libs

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -2,6 +2,8 @@
 using System.Xml.Linq;
 using static System.Console;
 using Packt.Shared;
+using DialectSoftware.Collections;
+using DialectSoftware.Collections.Generics;
 
 namespace AssembliesAndNamespaces
 {
@@ -9,10 +11,12 @@ namespace AssembliesAndNamespaces
     {
         static void Main(string[] args)
         {
+            // Part 1
             /* var doc = new XDocument();
             string s1 = "Hello";
             string s2 = "World"; */
-            Write("Enter a color value in hex: ");
+            // Part 2
+            /* Write("Enter a color value in hex: ");
             string hex = ReadLine();
             WriteLine("Is {0} a valid color value? {1}",
             arg0: hex, arg1: hex.IsValidHex());
@@ -23,7 +27,32 @@ namespace AssembliesAndNamespaces
             Write("Enter a password: ");
             string password = ReadLine();
             WriteLine("Is {0} a valid password? {1}",
-            arg0: password, arg1: password.IsValidPassword());
+            arg0: password, arg1: password.IsValidPassword()); */
+
+            // Part 3
+            var x = new Axis("x", 0, 10, 1);
+            var y = new Axis("y", 0, 4, 1);
+            var matrix = new Matrix<long>(new[] { x, y });
+            for (int i = 0; i < matrix.Axes[0].Points.Length; i++)
+            {
+                matrix.Axes[0].Points[i].Label = "x" + i.ToString();
+            }
+            for (int i = 0; i < matrix.Axes[1].Points.Length; i++)
+            {
+                matrix.Axes[1].Points[i].Label = "y" + i.ToString();
+            }
+            foreach (long[] c in matrix)
+            {
+                matrix[c] = c[0] + c[1];
+            }
+            foreach (long[] c in matrix)
+            {
+                WriteLine("{0},{1} ({2},{3}) = {4}",
+                matrix.Axes[0].Points[c[0]].Label,
+                matrix.Axes[1].Points[c[1]].Label,
+                c[0], c[1], matrix[c]);
+            }
+
         }
     }
 }
