96891ed45e57fa9830359812670341f8f052bff1
ZSteiger <z.steig93@gmail.com>
2019-12-05 16:36:41 -0500

    Exercise 7.1

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -56,3 +56,32 @@ namespace AssembliesAndNamespaces
         }
     }
 }
+
+/*
+------- Exercise 7.1 -------
+1. What is the difference between a namespace and an assembly?
+Assemblies are where the type is stored, a namespace is the address of that type.
+
+2. How do you reference another project in a .csproj file? A: In the ItemGroup tag, add a PackageReference tag then the name of the package in the Include property
+
+3. What is the difference between a package and a metapackage? Give an
+example of each. A: A package is a single assembly of the same name, i.e. System.Collections, which contains the System.Collections.dll assembly. A metapackage can be a package that installs a series of packages or boilerplate for a project depending on the needs of the project
+
+4. Which .NET type does the C# float alias represent? System.Single
+
+5. What is the difference between the packages named NETStandard.Library
+and Microsoft.NETCore.App? A: The NETStandard library is a collection of .NET APIs that are prescribed to be used and supported together, the NETCore.App is a set of APIs included in the .NET Core Application Model
+
+6. What is the difference between framework-dependent and self-contained
+deployments of .NET Core applications? A: Framework dependent might require some additional dependencies to be installed for the application to work, where as a self-contained application will have a large folder size, but is gauranteed to work. 
+
+7. What is a RID? A: A Runtime Identifier identifies the target platforms in which the application runs.
+
+8. What is the difference between the dotnet pack and dotnet publish
+commands? A: pack creates a NuGet package for the project, while publish compiles then publishes the project either with dependencies or as a self-contained application
+
+9. What types of applications written for the .NET Framework can be ported to
+.NET Core? A: ASP.NET Core MVC, ... Core Web API services, Console applications, Windows forms, windows presentation foundation, and Universal Windows Platform
+
+10. Can you use packages written for .NET Framework with .NET Core? A: Sometimes, though the compiler might through warnings, the code can still work in certain cases. 
+*/
