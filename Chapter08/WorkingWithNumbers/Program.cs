378724cc42faa689aedb8e6ed708c62edda96797
ZSteiger <z.steig93@gmail.com>
2019-12-05 17:03:24 -0500

    Chapter08 intro & working with big integers

---
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+using static System.Console;
+
+namespace WorkingWithNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var largest = ulong.MaxValue;
+            // the 40 represents right-align 40 characters, N0 means use thousand separators and zero decimal places
+            WriteLine($"{largest,40:N0}");
+            var atomsInTheUniverse = BigInteger.Parse("123456789012345678901234567890");
+            WriteLine($"{atomsInTheUniverse,40:N0}");
+        }
+    }
+}
